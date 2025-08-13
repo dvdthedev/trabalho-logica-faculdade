2ea048fb75b2d243b2a46aa73352efca1f319dd9
dvdthedev <dvd2.0@live.com>
2025-08-13 11:46:03 -0300

    criação de laços para fluxo do programa

--- cmdRunnerRestaurante.py
+++ cmdRunnerRestaurante.py
@@ -15,6 +15,16 @@ chaveM = 'M'
 chaveG = 'G'
 
 def imprimeLinhaPorTamanhoDeMarmita(tamanho,marmita1, marmita2):
+    """
+
+    Args:
+        tamanho: Obtem a tamanho da marita afim de utilizar visualmente e passar como parametro
+        marmita1: recebe um Objeto do tipo Marmita
+        marmita2: recebe um Objeto do tipo Marmita
+
+    Returns: exibe na tela uma linha com tamanho e valores das marmitas repassadas por parametro
+
+    """
     print(
         f'{3 * '-'}|     {tamanho}     |{7 * ' '} R${marmita1.getValor(tamanho):.2f}{7 * ' '}'
         f'|{7 * ' '} R${marmita2.getValor(tamanho)}{7 * ' '} |{3 * '-'}')
@@ -31,3 +41,55 @@ print(f'{3 * '-'}|  Tamanho  |  {marmitaDeBifeAcebolado.sabor}  |  {marmitaDeFra
 imprimeLinhaPorTamanhoDeMarmita('P',marmitaDeBifeAcebolado, marmitaDeFrango)
 imprimeLinhaPorTamanhoDeMarmita('M',marmitaDeBifeAcebolado, marmitaDeFrango)
 imprimeLinhaPorTamanhoDeMarmita('G',marmitaDeBifeAcebolado, marmitaDeFrango)
+
+print(f'{65 * '-'}')
+sabor = ''
+acumulador = 0
+nomeSabor = ''
+tamanhoDaMarmita = ''
+opcao = 'S'
+
+while opcao == 'S':
+    if opcao.upper() == 'N':
+        break
+
+    while True:
+        try:
+            sabor = input('Entre com o sabor desejado: (BA/FF)').upper()
+            if 'BA' == sabor:
+                nomeSabor = 'Bife Acebolado'
+                break
+            elif 'FF' == sabor:
+                nomeSabor = 'Filé de Frango'
+                break
+            else:
+                continue
+        except:
+            print('Sabor inválido, tente novamente.')
+
+    while True:
+        try:
+            tamanhoDaMarmita = input('Entre com a tamanho da marta: (P/M/G): ').upper()
+            if tamanhoDaMarmita == 'P':
+                break
+            if tamanhoDaMarmita == 'M':
+                break
+            if tamanhoDaMarmita == 'G':
+                break
+        except:
+            print('Tamanho inválido, tente novamente.')
+
+    if nomeSabor == 'Bife Acebolado':
+        if tamanhoDaMarmita == 'P':
+            acumulador += marmitaDeBifeAcebolado.getValor(tamanhoDaMarmita)
+
+    while True:
+        try:
+            opcao = input('Deseja mais alguma coisa? (S/N): ').upper()
+            if opcao.upper() == 'S':
+                break
+            if opcao.upper() == 'N':
+                break
+        except:
+            print('Opção inválida!')
+print(nomeSabor, tamanhoDaMarmita, acumulador)
