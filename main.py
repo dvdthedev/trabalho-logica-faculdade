213246d91f7703b59662e6b0c41c10981141b3d1
Deivid Rocha <deividzor@gmail.com>
2025-08-12 21:13:13 -0300

    atendendo exigencias 2

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ print('Bem vindo a loja do Deivid Alves da Rocha')
 valorDoPedido = 0
 quantidadeParcelas = 0
 
+#Laço de repetição para obrigar o usuário digitar um valor float válido para o pedido
 while True:
     try:
-        valorDoPedido = float(input('Insira o valor da parcela: R$'))
+        valorDoPedido = float(input('Insira o valor do pedido: R$'))
         if 1 <= valorDoPedido:
             break
         else:
             continue
     except:
         print('Valor inválido, digite só números e ponto')
-
+        
+#Laço de repetição para obrigar o usuário digitar um valor int válido de parcelas
 while True:
     try:
         quantidadeParcelas = int(input('Insira a quantidade de parcelas: '))
@@ -23,6 +25,15 @@ while True:
         print('Valor de parcela inválido')
 
 def print_parcela (valorTotal):
+    """
+    Args:
+        valorTotal : Utilizado para retorno do valor total e para o cálculo de parcelas
+        valorDaParcela : Utilizado para dividir o valorTotal por quantidadeDeParcelas afim de obter o valor de cada parcela.
+    Returns:
+        Nenhum: print com valores visuais para o usuário
+
+    """
+
     valorDaParcela = valorTotal / quantidadeParcelas
     print(f'O valor das parcelas é de R$ {valorDaParcela:.2f}')
     print(f'O valor total parcelado é de: R$ {valorTotal:.2f}')
@@ -40,7 +51,7 @@ elif quantidadeParcelas >= 6 and quantidadeParcelas < 9:
 elif quantidadeParcelas >= 9 and quantidadeParcelas < 13:
     valorTotalParcelado = valorDoPedido * (1 + (16 / 100))
     print_parcela((valorTotalParcelado))
-elif quantidadeParcelas >= 13:
+else:
     valorTotalParcelado = valorDoPedido * (1 + (32 / 100))
     print_parcela((valorTotalParcelado))
 
