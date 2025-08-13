f67c47ee06413baee9cee0e7d8d1f10006d592ae
dvdthedev <dvd2.0@live.com>
2025-08-13 15:53:01 -0300

    criando camiseteria

---
+++ camiseteria/main.py
@@ -0,0 +1,23 @@
+mcs = {'nome': 'Camiseta Manga Curta Simples', 'valor' : 1.8}
+mls = {'nome': 'Camiseta Manga Longa Simples', 'valor' : 2.1}
+mce = {'nome': 'Camiseta Manga Curta Com Estampa', 'valor' : 2.9}
+mle = {'nome': 'Camiseta Manga Longa Com Estampa', 'valor' : 3.2}
+
+frete = {'transportadora': 100.0 , 'sedex': 200.0, 'retirar': 0}
+
+opcao = ''
+
+# def escolha_modelo(camiseta):
+#     while True:
+#         try:
+#             opcao = input('Entre com o modelo desejado: '
+#                           'teste')
+#
+#
+
+
+print('Bem vindo a fábrica de Camisetas do Deivid Alves da Rocha')
+
+print('teste\n'
+      'teste\n'
+      'teste')
